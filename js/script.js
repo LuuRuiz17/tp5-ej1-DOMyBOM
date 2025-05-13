9391c253eb5c68ff6b9772206acf195f77646ebb
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-05-13 17:51:30 -0300

    Agregué la opción de comenzar el juego de nuevo

--- js/script.js
+++ js/script.js
@@ -5,36 +5,54 @@ indicarle con un alert si el numero que ingreso es mayor o menor al número mág
 Cuando el usuario adivine el numero mostrar un mensaje indicando al usuario que adivino el numero. */
 
 function generarNumero() {
-    let random = Math.floor(Math.random() * 100) + 1;
-    const input = document.getElementById("numeroAleatorio");
-    const divOculto = document.getElementById("oculto");
+    numeroMagico = Math.floor(Math.random() * 100) + 1;
+    const oculto = document.getElementById("oculto");
     const btnComenzar = document.getElementById("btnComenzar");
+    const mensajeVictoria = document.getElementById("mensajeVictoria");
+    const intentoNumero = document.getElementById("intentoNumero");
+    const numeroAleatorio = document.getElementById("numeroAleatorio");
+
+    oculto.classList.remove("d-none");
     btnComenzar.classList.add("d-none");
-    divOculto.classList.remove("d-none");
-    input.value = random;
-    console.log(random);
+    mensajeVictoria.classList.add("d-none");
+    intentoNumero.value = "";
+    numeroAleatorio.classList.add("d-none");
+    numeroAleatorio.value = "";
+
+  console.log(numeroMagico);
 }
 
 function adivinarNumero(e) {
-    e.preventDefault();
-    const input = document.getElementById("intentoNumero");
-    const intentoNumero = input.value;
-    const numeroAleatorio = document.getElementById("numeroAleatorio");
-    const mensajeVictoria = document.getElementById("mensajeVictoria");
-    if (intentoNumero == numeroAleatorio.value) {
-        numeroAleatorio.classList.remove("d-none");
-        mensajeVictoria.classList.remove("d-none");
-    }
-    else{
-        if(intentoNumero < numeroAleatorio.value){
-            alert("El número mágico es mayor que el que ingresaste.");
-            input.value = "";
-        }else{
-            alert("El número mágico es menor que el que ingresaste.");
-            input.value = "";
-        }
-    }
+  e.preventDefault();
+
+  const intentoInput = document.getElementById("intentoNumero");
+  const intento = parseInt(intentoInput.value);
+  const inputAleatorio = document.getElementById("numeroAleatorio");
+  const mensajeVictoria = document.getElementById("mensajeVictoria");
+  const btnComenzar = document.getElementById("btnComenzar");
+
+  if (isNaN(intento)) {
+    alert("Por favor, ingresa un número válido.");
+  }
+
+  if (intento === numeroMagico) {
+
+    inputAleatorio.value = numeroMagico;
+    inputAleatorio.classList.remove("d-none");
+    mensajeVictoria.classList.remove("d-none");
+    btnComenzar.textContent = "Intentar de nuevo";
+    btnComenzar.classList.remove("d-none");
+    btnComenzar.classList.add("mt-3");
+    
+  } else if (intento < numeroMagico) {
+    alert("El número mágico es mayor que el que ingresaste.");
+  } else {
+    alert("El número mágico es menor que el que ingresaste.");
+  }
+
+  intentoInput.value = "";
 }
 
-const formulario = document.querySelector("form");
-formulario.addEventListener("submit" , adivinarNumero);
+    let numeroMagico = 0;
+    const form = document.querySelector("form");
+    form.addEventListener("submit", adivinarNumero);
