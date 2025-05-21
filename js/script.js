723071a0322fc4309bab152fd586e166d7193703
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-05-21 00:10:36 -0300

    Hice que la página sea responsive

--- js/script.js
+++ js/script.js
@@ -42,6 +42,7 @@ function adivinarNumero(e) {
     mensajeVictoria.classList.remove("d-none");
     btnComenzar.textContent = "Intentar de nuevo";
     btnComenzar.classList.remove("d-none");
+    btnComenzar.classList.add("mb-3");
     
   } else if (intento < numeroMagico) {
     alert("El número mágico es mayor que el que ingresaste.");
